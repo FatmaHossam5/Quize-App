82fa1b1cd2d2c4fd038d3a2adf61e433c3911fe0
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2024-02-13 05:43:42 +0200

    responsive

--- src/Components/Students/Students.tsx
+++ src/Components/Students/Students.tsx
@@ -1,54 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import userImg from '../../assets/user img.png'
-import AddModal from '../../Shared/AddModal/AddModal';
+
 import axios from 'axios';
 import { useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
+
 import Loading from '../../Shared/Loading/Loading';
 
 import useAllStudents from '../../ApiUtls/useAllStudents';
 import SharedModal from '../../Shared/AddModal/AddModal';
+import { toast } from 'react-toastify';
 export default function Students() {
   const { headers } = useSelector((state: any) => state.userData)
   const { getAllStudents, loading, students } = useAllStudents()
   const Groups = ['Group1', 'Group2', 'Group3']
   const [showModal, setShowModal] = useState(false);
   const closeModal = () => setShowModal(false);
-  
- 
 
-  const [modalAction, setModalAction] = useState('add'); 
-  const[studentId,setStudentId]=useState(0)
 
-  const handleOpenModal = (action,studentId) => {
+
+  const [modalAction, setModalAction] = useState('add');
+  const [studentId, setStudentId] = useState(0)
+
+  const handleOpenModal = (action, studentId) => {
     setModalAction(action)
     setShowModal(true);
-   setStudentId(studentId)
-  
+    setStudentId(studentId)
+
   };
 
-  // const handleDelete=()=>{
-  //   if (!studentId) return; 
-  //   axios.delete(`https://upskilling-egypt.com:3005/api/student/${studentId}`,headers).then((response)=>{
-  //     console.log(response);
-      
-  //   }).catch((error)=>{
-  //     console.log(error);
-      
-  //   })
-  // }
+  const handleDelete = () => {
+    if (!studentId) return;
+    axios.delete(`https://upskilling-egypt.com:3005/api/student/${studentId}`, headers).then((response) => {
+      toast.success(response?.data?.message)
+      closeModal();
+      getAllStudents()
+    }).catch((error) => {
+      console.log(error);
+
+    })
+  }
   const saveChanges = () => {
     if (modalAction === 'add') {
-      // Handle add action
+      alert('hhhhhhh')
     } else if (modalAction === 'delete') {
- handleDelete();
-console.log('ddddddd');
+
+      handleDelete();
 
 
     }
     setShowModal(false);
   };
-  
+
   useEffect(() => {
     getAllStudents();
   }, [])
@@ -72,50 +74,50 @@ console.log('ddddddd');
               <button key={index} className={`mr-5 rounded-3xl border ${index === 0 ? 'bg-black text-white' : ''} border-black w-32 mt-4`}>{group}</button>
             </>)}
           </div>
-       
-<div>
-<SharedModal
-        show={showModal && modalAction === 'add'}
-        setShow={setShowModal}
-        title="Add New Student"
-        onSave={saveChanges}
-        body={(
-          <div className='px-32'>
-            {/* Add student form */}
-            {/* Name Input*/}
-            <div className='my-1'>
-              <div className='relative mt-2 rounded-md shadow-sm'>
-                <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 bg-red-300'>
-                  <span className='text-black sm:text-sm p-2'>Name</span>
+
+          <div>
+            <SharedModal
+              show={showModal && modalAction === 'add'}
+              setShow={setShowModal}
+              title="Add New Student"
+              onSave={saveChanges}
+              onClose={closeModal}
+              body={(
+                <div className='px-32'>
+                  {/* Add student form */}
+                  {/* Name Input*/}
+                  <div className='my-1'>
+                    <div className='relative mt-2 rounded-md shadow-sm text-center'>
+                      <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 bg-red-300'>
+                        <span className='text-black sm:text-sm p-2'>Name</span>
+                      </div>
+                      <input
+                        type='text'
+                        name='name'
+                        id='name'
+                        className='py-2 block w-full rounded-md border-0  px-28    text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                      />
+                    </div>
+                  </div>
+                  {/* Phone Input */}
+                  <div className='my-1'>
+                    <div className='relative mt-2 rounded-md shadow-sm  '>
+                      <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 bg-red-300'>
+                        <span className='text-black sm:text-sm p-2'>Phone</span>
+                      </div>
+                      <input
+                        type='text'
+                        name='phone'
+                        id='phone'
+                        className='py-2 block w-full rounded-md border-0 px-28 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                      />
+                      <div className='absolute inset-y-0 right-0 flex items-center'></div>
+                    </div>
+                  </div>
                 </div>
-                <input
-                  type='text'
-                  name='name'
-                  id='name'
-                  className='py-2 block w-full rounded-md border-0 px-28 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                />
-                <div className='absolute inset-y-0 right-0 flex items-center'></div>
-              </div>
-            </div>
-            {/* Phone Input */}
-            <div className='my-1'>
-              <div className='relative mt-2 rounded-md shadow-sm'>
-                <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 bg-red-300'>
-                  <span className='text-black sm:text-sm p-2'>Phone</span>
-                </div>
-                <input
-                  type='text'
-                  name='price'
-                  id='price'
-                  className='py-2 block w-full rounded-md border-0 px-28 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                />
-                <div className='absolute inset-y-0 right-0 flex items-center'></div>
-              </div>
-            </div>
+              )}
+            />
           </div>
-        )}
-      />
-</div>
           {students.length > 0 ? <div className="grid md:grid-cols-2 grid-cols-1 gap-4 mt-4 border mx-5 rounded-2xl pr-4 ">
             {
               students.map((student, index) => (
@@ -135,7 +137,7 @@ console.log('ddddddd');
                       <button>
                         <i className="fa-solid fa-circle-arrow-right mt-[5px]  pr-2  "></i>
                       </button>
-                      <button onClick={() => handleOpenModal('delete',student._id)}>
+                      <button onClick={() => handleOpenModal('delete', student._id)}>
                         <i className="fa-solid fa-trash mr-2 text-red-400"></i>
                       </button>
 
@@ -150,11 +152,24 @@ console.log('ddddddd');
           </div> : <div className=' text-6xl h-[50%] w-full  flex items-center justify-center py-5'><Loading /></div>}
 
 
-    
 
+          <SharedModal
+            show={showModal && modalAction === 'delete'}
+            setShow={setShowModal}
+            title="Delete Student"
+            onSave={saveChanges}
+            onClose={closeModal}
+            body={(
+              <div className='text-center'>
+                <p>Are you sure you want to delete this student?</p>
+                {/* <button onClick={saveChanges}  className='bg-red-300 w-40 py-2 mt-4 rounded-lg text-white'>Delete</button> */}
+              </div>
+            )}
+            buttonLable='Delete'
+          />
         </div>
       </div>
-     
+
     </>
   )
 }
--- src/Shared/AddModal/AddModal.tsx
+++ src/Shared/AddModal/AddModal.tsx
@@ -1,15 +1,111 @@
-import React from "react";
-import {
-  TEModal,
-  TEModalDialog,
-  TEModalContent,
-  TEModalHeader,
-  TEModalBody,
-  TEModalFooter,
-  TERipple,
+// import React from "react";
+// import {
+//   TEModal,
+//   TEModalDialog,
+//   TEModalContent,
+//   TEModalHeader,
+//   TEModalBody,
+//   TEModalFooter,
+//   TERipple,
 
-} from "tw-elements-react";
+// } from "tw-elements-react";
 
+// interface SharedModalProps {
+//   show: boolean;
+//   setShow: React.Dispatch<React.SetStateAction<boolean>>;
+//   title: string;
+//   body: string;
+//   onClose: () => void;
+//   onSave: () => void;
+  
+// }
+
+// const SharedModal: React.FC<SharedModalProps> = ({
+//   show,
+//   setShow,
+//   title,
+//   body,
+//   onClose,
+//   onSave,
+//   buttonLable,
+
+// }) => {
+//   return (
+//     <div className={`fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50  ${show ? 'flex ' : 'hidden'}`}>
+//     <div className=" ">
+//  <TEModal  className="w-[50%] flex justify-center items-center m-auto"show={show} setShow={setShow}>
+//       <TEModalDialog >
+//         <TEModalContent>
+//           <TEModalHeader className="py-3  ">
+//             <div>
+//             <h5 className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200">
+//               {title}
+//             </h5>
+//             </div>
+//         <div className="flex  ">
+//         <button
+//                   type="button"
+//                   className="ml-auto  box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+      
+//                   aria-label="Check"
+//                 >
+//                   <svg
+//                     xmlns="http://www.w3.org/2000/svg"
+//                     fill="none"
+//                     viewBox="0 0 24 24"
+//                     strokeWidth="1.5"
+//                     stroke="currentColor"
+//                     className="h-6 w-6 border-r-2 "
+                   
+//                   >
+//                     <path
+//                       strokeLinecap="round"
+//                       strokeLinejoin="round"
+//                       d="M5 13l4 4L19 7"
+//                     />
+//                   </svg>
+//                   </button>
+//         <button
+//               type="button"
+//               className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+//               onClick={onClose}
+//               aria-label="Close"
+//             >
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 strokeWidth="1.5"
+//                 stroke="currentColor"
+//                 className="h-6 w-6"
+//               >
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   d="M6 18L18 6M6 6l12 12"
+//                 />
+//               </svg>
+//             </button>
+         
+//         </div>
+          
+//           </TEModalHeader>
+//           <TEModalBody>
+//             {body}
+//    <button className="bg-blue-800" onClick={onSave}>{buttonLable}</button>
+//           </TEModalBody>
+     
+//         </TEModalContent>
+//       </TEModalDialog>
+//     </TEModal>
+//     </div>
+//     </div>
+//   );
+// };
+
+// export default SharedModal;
+
+import React from 'react'
 interface SharedModalProps {
   show: boolean;
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
@@ -27,78 +123,38 @@ const SharedModal: React.FC<SharedModalProps> = ({
   body,
   onClose,
   onSave,
+  buttonLable,
 
 }) => {
   return (
-    <div className={`fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50  ${show ? 'flex ' : 'hidden'}`}>
-    <div className=" ">
- <TEModal  className="w-[50%] flex justify-center items-center ml-80"show={show} setShow={setShow}>
-      <TEModalDialog >
-        <TEModalContent>
-          <TEModalHeader className="py-3  ">
-            <div>
-            <h5 className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200">
-              {title}
-            </h5>
-            </div>
-        <div className="flex  ">
-        <button
-                  type="button"
-                  className="ml-auto  box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-          
-                  aria-label="Check"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-6 w-6 border-r-2 "
-                   
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                  </button>
-        <button
-              type="button"
-              className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-              onClick={onClose}
-              aria-label="Close"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-         
-        </div>
-          
-          </TEModalHeader>
-          <TEModalBody>{body}
-   
-          </TEModalBody>
-     
-        </TEModalContent>
-      </TEModalDialog>
-    </TEModal>
+    <div className={`fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50  ${show ? 'flex justify-center items-center' : 'hidden'}`}>
+  <div  className='bg-white w-50 h-auto  rounded-lg' show={show} setShow={setShow} >
+    <div className='header flex justify-between border-b-2'>
+<div className='headerName mt-3 ml-2'>{title}</div>
+<div >
+                                    <button
+                                        className="border-l-2 p-3   "
+                                        onClick={onSave}
+                                    >
+                                        <i className="fa-solid fa-check"></i>
+                                    </button>
+                              
+                                    <button
+                                        className="  border-l-2 p-3  "
+                                        aria-label="Close"
+                                        onClick={onClose}
+                                    >
+                                        <i className="fa-solid fa-xmark"></i>
+                                    </button>
+                                </div>
     </div>
+<div className='py-4'>
+  {body}
+</div>
+  </div>
     </div>
   );
 };
 
 export default SharedModal;
+
